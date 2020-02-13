45feb90de45edb67e1d7fc02c9a389a315806baa
KovacZan <zan@ark.io>
2020-02-13 09:39:58 +0100

    chore: update comments

--- src/main/java/TransferExample.java
+++ src/main/java/TransferExample.java
@@ -23,17 +23,19 @@ public class TransferExample {
                 .recipient(recipientAddress)
                 .amount(amount)
                 .nonce(nonce)
-                .vendorField("Java \uD83D \uDD31 \uD83C \uDF7A")
+                .vendorField("Java \uD83D \uDD31 \uD83C \uDF7A") // not necessary to add this
                 .sign(passphrase)
                 .transaction;
 
         return actual;
     }
     public static long getNonce(Connection connection, String senderWallet) throws IOException {
+        // This is where we get nonce scrape out nonce from api
         return Long.valueOf (((LinkedTreeMap<String, Object>) connection.api().wallets.show(senderWallet).get("data")).get("nonce").toString());
     }
 
     public static void main(String[] args) throws IOException {
+        // Here we initialize recipientAddress, senderAddress and senderPassphrase
         String recipientAddress = "D8rr7B1d6TL6pf14LgMz4sKp1VBMs6YUYD";
         String senderAddress = "DBoKkzKAT6YdXhyv1mvcKEg97JxiRqMuK2";
         String senderPassphrase = "blanket swing carpet interest assault mom you fault float cave category cradle";
@@ -41,20 +43,26 @@ public class TransferExample {
         // This is where we configure our network and connect to the node
         Network.set(new Devnet());
 
+        // This is where we make configurations for connection
         HashMap<String, Object> map = new HashMap<>();
-        map.put("host", "http://137.74.27.246:4003/api/");
+        map.put("host", "https://dexplorer.ark.io/api/");
         map.put("content-type","application/json");
+        // Here we make a connection to configured node
         Connection connection2 = new Connection(map);
 
         // This is where we get senders nonce and increment it by one
         long nonce = getNonce(connection2,senderAddress) + 1;
 
+        // Here we initialize payload
         ArrayList<HashMap> payload = new ArrayList<>();
-        Transaction transfer1 = CreateTransferTransaction(10,
+
+        // This is where we call our Create method and get our transfer transaction
+        Transaction transfer1 = CreateTransferTransaction(10, // amount of arktoshis we want to send
                         recipientAddress,
                         senderPassphrase,
                         nonce
                 );
+        // Here we add transaction to payload -it is possible to add multiple transaction to payload
         payload.add(transfer1.toHashMap());
 
         // This is where we send our transaction to node
